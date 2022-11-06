8d8fd51828bdde278b45e3a9a07b898c6bb35722
Mher21 <mher.araqelyan.ec@gmail.com>
2022-11-06 23:43:05 +0400

    cout by levels

--- map.cpp
+++ map.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+static int u=1;
  class list {
         public:
         int value=NULL;
@@ -27,19 +28,64 @@ using namespace std;
 
         }
     };
-int main(){   
+    int maxHeight(list* x ) {
+        if(x==NULL)
+        return 0;
+        else{
+            int heightLeft=maxHeight(x->left);
+            int heightRight=maxHeight(x->right);
+            return max(heightLeft,heightRight)+1;
+
+        }
+
+
+    }
+    int ByLevels(list* x){
+       /* if(x!=NULL)
         
+        cout<<"----"<<x->value<<"-----"<<;*/
+       
+        
+        if(x->left!=NULL)
+        cout<<"-----"<<x->left->value<<"-----";
+         if(x->right!=NULL){
+        cout<<"----"<<x->right->value<<"-----";
+        cout<<endl;}
+       else return 0;
+     
+           
+            
+
+            ByLevels(x->left);
+            
+            ByLevels(x->right);
+            cout<<endl;
+            
+           
+            
+        }
+
+
+    
+   
+int main(){   
+      
 list a;
+list*p=&a; 
 a.insert(15);
 a.insert(30);
 a.insert(5);
 a.left->insert(6);
 a.left->insert(2);
+a.right->insert(16);
+a.right->insert(32);
 
 cout<<a.value<<"  ::first element"<<endl;
 cout<<a.right->value<<"  ::second element"<<endl;
 cout<<a.left->value<<"  :: third element"<<endl;
 cout<<a.left->left->value<<endl;
 cout<<a.left->right->value<<endl;
+cout<<maxHeight(p)<<endl;
+cout<<ByLevels(p);
 return 0;
 }
